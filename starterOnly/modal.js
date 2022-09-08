dcccc7d7d645d7fd11fc248a5ef2f8441c43cad3
swanndolia <swanndolia@gmail.com>
2022-09-09 00:40:37 +0200

    entrées et validification formulaire ok; edit html

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,52 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
 
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const brithDate = document.querySelector("#birthdate");
+const howManyConstest = document.querySelector("#quantity");
+const acceptCGU = document.querySelector("#checkbox-cgu");
+const acceptNews = document.querySelector("#checkbox-news");
+
+//get all location as array instead nodelist to process with some later
+const contestList = [...document.querySelectorAll('[name="location"]')];
+
+//modal informations are valids
+function modalsInformationAreValids(event){
+  let errorMessage = "";
+  //not really valid cause some special character are valids in locale part of email, this is more for demonstration cause recomendation are to just check for something like x@x.x 
+  let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  if(firstName.value.length < 2){
+    event.preventDefault();
+    errorMessage += "Vérifier le champ prénom, il doit comporter au moins deux charactères\n\n"
+  }
+  if(lastName.value.length < 2){
+    event.preventDefault();
+    errorMessage += "Vérifier le champ nom, il doit comporter au moins deux charactères\n\n"
+  }
+  if(!email.value.match(emailRegex)){
+    event.preventDefault();
+    errorMessage += "Vérifier le champ email, celui ci n'est pas valide\n\n"
+  }
+  if(isNaN(howManyConstest.value)){
+    event.preventDefault();
+    errorMessage += "Veuillez indiquer le nombre de tournoi au quel vous avez participé\n\n"
+  }
+  if(!acceptCGU.checked){
+    event.preventDefault();
+    errorMessage += "Vous devez accepter les CGUs\n\n"
+  }
+  if(!contestList.some(element => element.checked)){
+    event.preventDefault();
+    errorMessage += "Veuiller choisir le tournoi au quel vous voulez participez\n\n"
+  }
+  if(errorMessage != ""){
+    alert(errorMessage);
+    errorMessage = "";
+  } 
+}
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -21,11 +67,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//close modal form
+//close modal event
 modalCloseBtn.addEventListener("click", closeModal);
 
+//close modal form
 function closeModal(){
   modalbg.style.display = "none";
 }
-
-
